b63dbc16856ec71e7b65eae4da42956e229a2d6b
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-10-05 10:07:17 -0400

    fixed factory girl mixup OOPS

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,10 +1,6 @@
 require 'factory_girl_rails'
 
 FactoryGirl.define do
-  factory :user do
-    sequence(:username) { |n| "PartyBoy200#{n}" }
-    sequence(:email) { |n| "partyboy200#{n}@example.com"}
-  end
 
   factory :devtool do
     sequence(:title) { |n| "Tool #{n}" }
