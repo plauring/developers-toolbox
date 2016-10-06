e5044e8d4fbee451c766ce830cc9e57fcaa623ea
Brady Tatro <bradytatro@gmail.com>
2016-10-06 15:15:09 -0400

    Tests created, ajax functionality to be added

--- spec/features/votes_on_reviews/01_user_can_vote_spec.rb
+++ spec/features/votes_on_reviews/01_user_can_vote_spec.rb
@@ -1,37 +1,74 @@
 require 'rails_helper'
 
 feature 'A user can see vote buttons on reviews' do
-let!(:user) {User.create(name: "partybody200", email:"2hard2party@example.com", password:"123qwe")}
-let!(:user1) {User.create(name: "partybody2001", email:"2hard2party@example.com1", password:"123qwe1")}
-let!(:user2) {User.create(name: "partybody2002", email:"2hard2party@example.com2", password:"123qwe2")}
-
 let!(:devtool1) {FactoryGirl.create(:devtool, id: 1)}
 let!(:review) {FactoryGirl.create(:review, devtool: devtool1)}
+
   context 'An authenticated user can click the buttons' do
+    let!(:user) {User.create(name: "partybody200", email:"2hard2party@example.com", password:"123qwe")}
     before { visit devtool_path(devtool1.id) }
+    
     scenario 'The user clicks upvote' do
       click_button('upvote')
       visit devtool_path(devtool1.id)
-          save_and_open_page
-      expect(page).to have_content("3 upvote")
-
+      expect(page).to have_content("1 upvote")
+      expect(page).to have_content("sum of votes: 1")
       click_button('upvote')
       visit devtool_path(devtool1.id)
-      expect(page).to have_content("")
+      expect(page).to have_content("0 upvote")
+      expect(page).to have_content("sum of votes: 0")
     end
+
     scenario 'The user clicks downvote' do
       click_button("downvote")
       visit devtool_path(devtool1.id)
-      expect(page).to have_content("3 downvote")
+      expect(page).to have_content("1 downvote")
+      expect(page).to have_content("sum of votes: -1")
+      click_button("downvote")
+      visit devtool_path(devtool1.id)
+      expect(page).to have_content("0 downvote")
+      expect(page).to have_content("sum of votes: 0")
     end
-    scenario 'The user clicks upvote a second time' do
-      # foo
+
+    scenario 'The user clicks downvote then upvote' do
+      click_button("downvote")
+      visit devtool_path(devtool1.id)
+      expect(page).to have_content("1 downvote")
+      expect(page).to have_content("sum of votes: -1")
+      click_button("upvote")
+      visit devtool_path(devtool1.id)
+      expect(page).to have_content("1 upvote")
+      expect(page).to have_content("sum of votes: 1")
     end
-    scenario 'The user clicks downvote a second time' do
-      # foo
+
+    scenario 'The user clicks upvote then downvote' do
+      click_button("upvote")
+      visit devtool_path(devtool1.id)
+      expect(page).to have_content("1 upvote")
+      expect(page).to have_content("sum of votes: 1")
+      click_button("downvote")
+      visit devtool_path(devtool1.id)
+      expect(page).to have_content("1 downvote")
+      expect(page).to have_content("sum of votes: -1")
     end
-    scenario 'User changes their vote' do
-      # foo
+
+    context 'More users click up/down vote' do
+      let!(:user) {User.create(name: "partybody200", email:"2hard2party@example.com", password:"123qwe")}
+      let!(:user1) {User.create(name: "partybody2001", email:"2hard2party@example.com1", password:"123qwe1")}
+
+      scenario 'Two users vote up' do
+        click_button('upvote')
+        visit devtool_path(devtool1.id)
+        expect(page).to have_content("2 upvote")
+        expect(page).to have_content("sum of votes: 2")
+      end
+
+      scenario 'Two users vote down' do
+        click_button('downvote')
+        visit devtool_path(devtool1.id)
+        expect(page).to have_content("2 downvote")
+        expect(page).to have_content("sum of votes: -2")
+      end
     end
   end
 end
