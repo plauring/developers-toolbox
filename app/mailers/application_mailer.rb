39b3d08b3921dfdcc3346b8c7a3f326e237506bd
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-10-11 15:00:28 -0400

    WIP

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "\"Devtool Reviews\" <no-reply@example.com"
   layout 'mailer'
 end
---
+++ app/mailers/review_mailer.rb
@@ -0,0 +1,5 @@
+class ReviewMailer < ApplicationMailer
+  def new_review(review)
+    # Build the email in here...
+  end
+end
