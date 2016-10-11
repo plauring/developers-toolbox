03939c5b9bf0fe74fbfc8aac550119884944fff4
PETER LAURING <PLAURING@GMAIL.COM>
2016-10-11 13:08:50 -0400

    finished up ajax functionality for upvote/downvote

--- app/assets/javascripts/votes_ajax.js
+++ app/assets/javascripts/votes_ajax.js
@@ -1,41 +1,41 @@
 $(document).ready (function() {
-$('.upvoteButton').on('click', function(event) {
-  event.preventDefault();
-  var targetReview = this.parentElement.action
 
-  var url = window.location.href;
+  $(`.upvoteButton`).on('click', function(event) {
+    event.preventDefault();
+    var targetReview = this.parentElement.action;
+    var currentTarget = $(event.currentTarget);
+    var id = this.id;
 
-  var request = $.ajax({
-    url: `${targetReview}.json`,
-    method: 'POST',
+    var request = $.ajax({
+      url: `${targetReview}.json`,
+      method: 'POST',
 
-  });
-
-  request.done(function(data) {
-    document.getElementById("upcount").innerHTML = `${data.upvotes} upvotes`;
-    document.getElementById("downcount").innerHTML = `${data.downvotes} downvotes`;
-    document.getElementById("sum").innerHTML = `sum of votes: ${data.sumvotes}`;
-  });
-
-});
-
-$('.downvoteButton').on('click', function(event) {
-  event.preventDefault();
-  var targetReview = this.parentElement.action
-
-  var url = window.location.href;
-
-  var request = $.ajax({
-    url: `${targetReview}.json`,
-    method: 'POST',
+    });
+    request.done(function(data) {
+      document.getElementById(`upcount${id}`).innerHTML = `${data.upvotes} upvotes`;
+      document.getElementById(`downcount${id}`).innerHTML = `${data.downvotes} downvotes`;
+      document.getElementById(`sum${id}`).innerHTML = `sum of votes: ${data.sumvotes}`;
+    });
 
   });
 
-  request.done(function(data) {
-    document.getElementById("upcount").innerHTML = `${data.upvotes} upvotes`;
-    document.getElementById("downcount").innerHTML = `${data.downvotes} downvotes`
-    document.getElementById("sum").innerHTML = `sum of votes: ${data.sumvotes}`;
-  });
+  $(`.downvoteButton`).on('click', function(event) {
+    event.preventDefault();
+    var targetReview = this.parentElement.action
+    var currentTarget = $(event.currentTarget);
+    var id = this.id;
 
-});
+    var request = $.ajax({
+      url: `${targetReview}.json`,
+      method: 'POST',
+
+    });
+
+    request.done(function(data) {
+      document.getElementById(`upcount${id}`).innerHTML = `${data.upvotes} upvotes`;
+      document.getElementById(`downcount${id}`).innerHTML = `${data.downvotes} downvotes`;
+      document.getElementById(`sum${id}`).innerHTML = `sum of votes: ${data.sumvotes}`;
+    });
+
+  });
 });
