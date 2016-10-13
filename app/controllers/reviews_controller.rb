3ecabd1963823b87c55ffb9c2fd3e06714d1471a
PETER LAURING <PLAURING@GMAIL.COM>
2016-10-13 10:09:44 -0400

    fixed hound indentation errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -28,8 +28,8 @@ class ReviewsController < ApplicationController
 
   def upvote
     @review = Review.find(params[:id])
-      @users = current_user
-      @user = @users.id
+    @users = current_user
+    @user = @users.id
     if Vote.find_by(user_id: @user, review_id: @review.id).nil?
       Vote.create(user_id: @user, review_id: @review.id, status: true)
     else
@@ -45,52 +45,52 @@ class ReviewsController < ApplicationController
     @upvote_count = []
     @downvote_count = []
 
-      @votes_for_review.each do |vote|
-        if vote.status == true
-          @upvote_count << vote
-        else
-          @downvote_count << vote
-        end
+    @votes_for_review.each do |vote|
+      if vote.status == true
+        @upvote_count << vote
+      else
+        @downvote_count << vote
       end
+    end
 
-      respond_to do |format|
-        format.html { redirect_to :back }
-        format.json { render :json => { upvotes: @upvote_count.length, downvotes: @downvote_count.length, sumvotes: (@upvote_count.length - @downvote_count.length) } }
-        format.js
-      end
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { render :json => { upvotes: @upvote_count.length, downvotes: @downvote_count.length, sumvotes: (@upvote_count.length - @downvote_count.length) } }
+      format.js
+    end
   end
 
   def downvote
     @review = Review.find(params[:id])
     @users = current_user
     @user = @users.id
-      if Vote.find_by(user_id: @user, review_id: @review.id).nil?
-        Vote.create(user_id: @user, review_id: @review.id, status: false)
+    if Vote.find_by(user_id: @user, review_id: @review.id).nil?
+      Vote.create(user_id: @user, review_id: @review.id, status: false)
+    else
+      if Vote.find_by(user_id: @user, review_id: @review.id).status == false
+        Vote.find_by(user_id: @user, review_id: @review.id).destroy
       else
-        if Vote.find_by(user_id: @user, review_id: @review.id).status == false
-          Vote.find_by(user_id: @user, review_id: @review.id).destroy
-        else
-          Vote.find_by(user_id: @user, review_id: @review.id).update(status: false)
-        end
+        Vote.find_by(user_id: @user, review_id: @review.id).update(status: false)
       end
+    end
 
-      @votes_for_review = @review.votes
-      @downvote_count = []
-      @upvote_count = []
+    @votes_for_review = @review.votes
+    @downvote_count = []
+    @upvote_count = []
 
-      @votes_for_review.each do |vote|
-        if vote.status == false
-          @downvote_count << vote
-        else
-          @upvote_count << vote
-        end
+    @votes_for_review.each do |vote|
+      if vote.status == false
+        @downvote_count << vote
+      else
+        @upvote_count << vote
       end
+    end
 
-      respond_to do |format|
-        format.html { redirect_to :back }
-        format.json { render :json => { downvotes: @downvote_count.length, upvotes: @upvote_count.length, sumvotes: (@upvote_count.length - @downvote_count.length) } }
-        format.js
-      end
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { render :json => { downvotes: @downvote_count.length, upvotes: @upvote_count.length, sumvotes: (@upvote_count.length - @downvote_count.length) } }
+      format.js
+    end
   end
 
   def destroy
