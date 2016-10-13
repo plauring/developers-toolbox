665319f4d23e088629f05b783fb401088a46fec4
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-10-13 10:46:27 -0400

    struggling with carrierwave, carrierwave.rb and .env have been altered and realtered. to no avail

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -7,12 +7,12 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.test?
-  #   storage :file
-  # else
-  #   storage :fog
-  # end
-  storage :file
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
